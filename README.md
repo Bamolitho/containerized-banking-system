0fed2df79c03b3a8614491a5ce3601394517342e
Bamolitho <Bamolitho@users.noreply.github.com>
2025-10-19 16:29:01 -0400

    Oct 19, 2025

--- README.md
+++ README.md
@@ -11,34 +11,32 @@ Application bancaire containérizé qui permet à un utilisateur de : créer, co
 Application bancaire qui permet de créer ou supprimer un compte et réaliser des transactions
 
 ​    Paramètres clés à utiliser pour se connecter à un compte : 
-        - Un id unique de 6 chiffres (généré par le système)
-                - Et un mot de passe (choisi par l'usager)
-        
-        - Une base de données SQLite pour stocker les infos principales
-            - Utilisation de Flask, HTML / CSS / JS  pour créer une interface web
+   - Un id unique de 6 chiffres (généré par le système)
+   - un mot de passe (choisi par l'usager)
+
+- Une base de données SQLite pour stocker les infos principales
+- Utilisation de Flask, HTML / CSS / JS  pour créer une interface web
 
 # Structure du répertoire : 
 
+```bash
 containerized-banking-system/
 ├── .gitignore
 ├── Dockerfile
-├── db.db 		# sera créée automatiquement
-├
-├── compte_bancaire.py     # Contient la classe principale et les méthodes creerCompte(), deposerArgent()...  
-├
-├── app.py
-├
-├── README.md          
-├── requirements.txt  # Les librairies Python utilisés, aucune installation nécessaire
-└── web/              # Répertoire des fichiers pour l'interface web 
-
-​	├── style.css
-
-​	├── style.js
-
-​        └── index.html    # Page d'accueil par défaut
-
-
+├── db.db                    # Base de données SQLite (créée automatiquement)
+│
+├── compte_bancaire.py       # Classe principale avec les fonctions creerCompte(), deposerArgent(), etc.
+│
+├── app.py                   # Application principale (API / logique métier)
+│
+├── README.md
+├── requirements.txt          # Librairies Python nécessaires (aucune installation manuelle requise)
+│
+└── web/                      # Fichiers pour l’interface web
+    ├── style.css
+    ├── style.js
+    └── index.html            # Page d’accueil par défaut
+```
 
 ## Installation et lancement
 
@@ -79,3 +77,63 @@ containerized-banking-system/
 ```
 
 # Scénario de test complet
+
+
+
+# Diagramme de classes
+
+```basic
++---------------------------+
+|    CompteBancaire         |
++---------------------------+
+| - __liste_identifiants    |
+| - __transactions_record   |
+| - __solde                 |
++---------------------------+
+| + creerCompte()           |
+| + seConnecter()           |
+| + consulterCompte()       |
+| + deposerArgent()         |
+| + retirerArgent()         |
+| + afficher_historique()   |
+| + effacerCompte()         |
++---------------------------+
+        ▲
+        │ uses
+        │
++---------------------------+
+|    MenuBancaire           |
++---------------------------+
+| + afficher_menu_non_utilisateur() |
+| + afficher_menu_utilisateur()     |
+| + get_into_main_menu()            |
+| + menu()                          |
++-----------------------------------+
+```
+
+MenuBancaire utilise un objet CompteBancaire
+
+### Relations (simplifiées)
+
+```basic
+main()
+  └──> menu()
+        ├──> afficher_menu_non_utilisateur()
+        ├──> [Choix utilisateur]
+        │     ├── (1) creerCompte()
+        │     ├── (2) seConnecter(id, password)
+        │     │       └──> get_into_main_menu()
+        │     │             ├──> afficher_menu_utilisateur()
+        │     │             ├── [Choix utilisateur]
+        │     │             │     ├── (1) deposerArgent()
+        │     │             │     ├── (2) retirerArgent()
+        │     │             │     ├── (3) consulterCompte()
+        │     │             │     ├── (4) effacerCompte(id, password)
+        │     │             │     └── (5) afficher_historique()
+        │     │             └──> retour au menu principal
+        │     └── (0) Quitter le programme
+        └──> Fin du programme
+```
+
+## Références
+
--- compte_bancaire.py
+++ compte_bancaire.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
+from datetime import datetime
+
 class CompteBancaire:
     def __init__(self):
         self.__liste_identifiants = []
         self.__transactions_record = []
+        self.__solde = 0.0
     
     def creerCompte(self):
         nom = input("Votre nom : ")
@@ -18,126 +21,146 @@ class CompteBancaire:
         new = {"nom":nom, "id":id, "password":password}
         self.__liste_identifiants.append(new)
 
-        print(f"Compte créé avec succès. Votre ID est {id}")
-        print(self.__liste_identifiants)
-
+        return id, password
 
     def seConnecter(self, id, password):
-        pass
+        for i in range(len(self.__liste_identifiants)) :
+            if id == self.__liste_identifiants[i]['id'] and password == self.__liste_identifiants[i]["password"]:
+                return 200
+        return 404
 
     def consulterCompte(self):
-        pass
+        print(f"Solde actuel : {self.__solde}")
     
     def deposerArgent(self, montant) :
         if(montant > 0.0) :
             self.__solde += montant
-            self.__transactions_record.append(f"Depot de {montant} euros => solde = {self.__solde}")
+            self.__transactions_record.append(
+                f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Depot de {montant} euros => solde = {self.__solde}"
+                )
         else :
-            print("Montant doit etre positif")
+            print("Montant doit être positif")
     
     def retirerArgent(self, montant) :
         if((self.__solde  > montant) & (montant > 0.0)):
             self.__solde -= montant 
-            self.__transactions_record.append(f"Retrait de {montant} euros => solde = {self.__solde}")
+            self.__transactions_record.append(
+                f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Retrait de {montant} euros => solde = {self.__solde}"
+                )
         elif(montant <= 0.0):
-            print("Montant doit etre positif")
+            print("Montant doit être positif")
         else:
             print("Insufficient funds")
     
     def afficher_historique(self) :
         if self.__transactions_record :
-            print("Transactions record : ")
+            print("\n====================== Historique des transactions ========================")
             for transaction in self.__transactions_record:
-                print(transaction)
+                print(f"• {transaction}")
+            print("============================================================================")
         else:
             print("No transactions yet")
     
-    def effacerCompte():
-        pass
+    def effacerCompte(self, id, password):
+        for compte in self.__liste_identifiants:
+            if compte["id"] == id and compte["password"] == password:
+                confirmation = input("Confirmez la suppression (o/n) : ")
+                if confirmation.lower() == "o":
+                    self.__liste_identifiants.remove(compte)
+                    return 200
+                else:
+                    return 403
+        return 404
+
     
-    def __str__ (self) -> str :
-        return f"Compte de {self.__nomProprietaire} de solde {self.__solde}"
-    
+    # def __str__ (self) -> str :
+    #     return f"Compte de {self.__nomProprietaire} de solde {self.__solde}"
 
-#client1 = CompteBancaire("Amolitho", 2025)
-#print(client1) # different de print(client1.__str__)
-#client1.afficher_historique()
+class MenuBancaire:
+    def afficher_menu_non_utilisateur(self):
+        print("\n=== Menu ===")
+        print("1. Créer un compte")
+        print("2. Se connecter")
+        print("0. Quitter")
 
-
-# client2 = CompteBancaire("BALDE") # Erreur !!! (on doit passer tous les arguments)
-
-#client1.deposer(75)
-#print(client1)
-#client1.afficher_historique()
-
-#client1.retirer(100)
-#print(client1)
-
-#print("\n")
-#client1.afficher_historique()
-
-def afficher_menu_non_utilisateur():
-    print("\n=== Menu ===")
-    print("1. Créer un compte")
-    print("2. Se connecter")
-    print("0. Quitter")
-
-def afficher_menu_utilisateur():
-    print("\n=== Menu ===")
-    print("1. Déposer de l'argent")
-    print("2. Retirer de l'argent")
-    print("3. Consulter compte")
-    print("4. Effacer compte")
-    print("5. Afficher l'historique des transactions")
-    print("0. Quitter")
+    def afficher_menu_utilisateur(self):
+        print("\n=== Menu ===")
+        print("1. Déposer de l'argent")
+        print("2. Retirer de l'argent")
+        print("3. Consulter compte")
+        print("4. Effacer compte")
+        print("5. Afficher l'historique des transactions")
+        print("0. Se déconnecter")
 
 
-def get_into_main_menu(compte):
-    while True:
+    def get_into_main_menu(self, compte):
+        while True:
 
-        afficher_menu_utilisateur()
-        choix = input("Choisissez une option : ")
+            self.afficher_menu_utilisateur()
+            choix = input("Choisissez une option : ")
 
-        if choix == '1':
-            montant = float(input("Entrez le montant a deposer : "))
-            compte.deposerArgent(montant)
-        elif choix == '2':
-            montant = float(input("Entrez le montant a retirer : "))
-            compte.retirerArgent(montant)
-        elif choix == '3':
-            compte.consulterCompte()
-        elif choix == '4': 
-            compte.effacerCompte()
-        elif choix == '5':
-            compte.afficher_historique()
-        elif choix == '0':
-            print("Merci d'avoir utilisé notre service bancaire.")
-            break
-        else:
-            print("Choix invalide. Veuillez reessayer.")
+            if choix == '1': # Déposer argent
+                montant = float(input("Entrez le montant a deposer : "))
+                compte.deposerArgent(montant)
 
-def menu():
-    compte = CompteBancaire()
+            elif choix == '2': # Rétirer argent
+                montant = float(input("Entrez le montant a retirer : "))
+                compte.retirerArgent(montant)
 
-    while True:
-        afficher_menu_non_utilisateur()
-        choix = input("Choisissez une option : ")
+            elif choix == '3': # Consulter compte
+                compte.consulterCompte()
 
-        if choix == '1': # Créer compte bancaire
-            compte.creerCompte()
-        elif choix == '2': # Se connecter
-            id = int(input("Votre id : "))
-            password = input("Votre password : ")
-            status = compte.seConnecter(id, password)
-            if status == 200 :
-                get_into_main_menu(compte)
-            else : 
-                print(f"ID ou password incorrect")
-        elif choix == '0':
-            print("Merci d'avoir utilisé notre service bancaire.")
-            break
-        else:
-            print("Choix invalide. Veuillez reessayer.")
+            elif choix == '4': # Effacer compte
+                print(f"[!] Cette action est irréversible")
+                id = int(input("Entrez votre ID : "))
+                password = input("Entrez votre password : ")
+                status = compte.effacerCompte(id, password)
+                if status == 200 :
+                    print(f"Compte {id} supprimé avec succès.")
+                    break
+                elif status == 403 : 
+                    print("Suppression annulée.")
+                else : 
+                    print("Identifiants incorrects. Suppression échouée.")
+
+            elif choix == '5': # Afficher historique
+                compte.afficher_historique()
+
+            elif choix == '0': # Se déconnecter
+                print("Merci d'avoir utilisé notre service bancaire.")
+                break
+
+            else:
+                print("Choix invalide. Veuillez reessayer.")
+
+    def menu(self):
+        compte = CompteBancaire()
+
+        while True:
+            self.afficher_menu_non_utilisateur()
+            choix = input("Choisissez une option : ")
+
+            if choix == '1': # Créer compte bancaire
+                id, password = compte.creerCompte()
+                print(f"Compte créé avec succès. Votre ID est {id} et password = {password}")
+                self.get_into_main_menu(compte)
+
+            elif choix == '2': # Se connecter
+                id = int(input("Votre ID : "))
+                password = input("Votre password : ")
+                status = compte.seConnecter(id, password)
+                if status == 200 :
+                    self.get_into_main_menu(compte)
+                else : 
+                    print("Identifiants incorrects. Connexion échouée.")
+
+            elif choix == '0': # Quitter
+                print("Merci d'avoir utilisé notre service bancaire.")
+                break
+
+            else:
+                print("Choix invalide. Veuillez reessayer.")
 
 if __name__ == "__main__":
-    menu()
+    menu_bancaire = MenuBancaire()
+    menu_bancaire.menu()
